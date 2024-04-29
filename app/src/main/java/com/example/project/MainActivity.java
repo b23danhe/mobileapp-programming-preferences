d069a63789985b82926fbd1a48ff829498337d1e
b23danhe <b23danhe@student.his.se>
2024-04-29 22:42:34 +0200

    Lagt till en knapp för att starta SecondActivity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     private SharedPreferences myPreferenceRef;
     private SharedPreferences.Editor myPreferenceEditor;
+    private Button newScreenButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +24,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       // SharedPreferences myPreferenceRef = getPreferences("MyPreferencesName", MODE_PRIVATE);
-       // SharedPreferences.Editor myPreferenceEditor = myPreferenceRef.edit();
-        myPreferenceRef = getPreferences(MODE_PRIVATE);
-        myPreferenceEditor = myPreferenceRef.edit();
-
-        TextView prefTextRef = new TextView(this);
-        prefTextRef = (TextView)findViewById(R.id.prefText);
-        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
+        newScreenButton = findViewById(R.id.newScreenButton);
+        newScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
